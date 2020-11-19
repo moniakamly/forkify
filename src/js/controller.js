b1857452a4b63475c52e9d5974e5a4a940ac9423
moniakamly <moniakamli95@gmail.com>
2020-11-19 11:16:53 +0100

    handling error and succes messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,14 +6,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -33,12 +25,13 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
     
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-controlRecipes();
+//controlRecipes();
 
-['hashchage', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes)
+}
+init();
