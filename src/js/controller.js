42505285a9a13b958fe3f3729e2b4678e2f4e33b
moniakamly <moniakamli95@gmail.com>
2020-11-20 11:58:12 +0100

    Implementing bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resulstView from './views/resultsView.js';
 import paginationtView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 
 import 'core-js/stable';
@@ -26,6 +27,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resulstView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // Loading recipe
     await model.loadRecipe(id);
@@ -82,9 +84,22 @@ const controlServings = function(newServings) {
   recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = function() {
+  // 1) Add/Remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // 2) Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddbookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationtView.addHandlerClick(controlPagination);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
         page: 1,
         resultsPerPage: RES_PER_PAGE,
     },
+    bookmarks: [],
 };
 
 
@@ -26,7 +27,11 @@ export const loadRecipe = async function (id) {
           servings: recipe.servings,
           cookingTime: recipe.cooking_time,
           ingredients: recipe.ingredients,
-        }
+        };
+
+        if (state.bookmarks.some(bookmark => bookmark.id === id)) 
+            state.recipe.bookmarked = true; 
+            else state.recipe.bookmarked = false; 
     } catch (err) {
         console.error(err);
         throw err;
@@ -71,4 +76,22 @@ export const updateServings = function(newServings) {
     });
 
     state.recipe.servings = newServings;
+}
+
+export const addBookmark = function(recipe) {
+    //Add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmarked
+    if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+}
+
+
+export const deleteBookmark = function(id) {
+    // Delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    // Mark current recipe as not bookmarked
+    if (id === state.recipe.id) state.recipe.bookmarked = false;
 }
